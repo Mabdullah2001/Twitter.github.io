de0313719d274ea38db8a02eb58dfcf6f22daa96
muhammad abdullah <chaudryabdullah2001@gmail.com>
2022-09-23 10:47:20 -0700

    --Navbar slider

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 function openNav() {
   document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("nav").style.display = "block";
+  document.getElementById("hamb").style.display = "none";
   document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("close").style.display = "block";
+
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("nav").style.display = "none";
+  document.getElementById("hamb").style.display = "block";
+  document.getElementById("close").style.display = "none";
+
 }
 
